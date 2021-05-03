15cb305da4e85e585d239bba773a45e6f37b47d2
Luiz Lopes <luizlopes12@outlook.com>
2021-05-03 18:08:46 -0300

    Revert "fazendo o close button"
    This reverts commit 9633d7b4b2d625576e178c19a0bcdab87c726500.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,3 @@ function abrir(){
     var menuOpen = document.querySelector('.menu-open');
     menuOpen.setAttribute("id", "btn-menu");
 }
-function fechar(){
-    var menuOpen = document.querySelector('.menu-open');
-    menuOpen.removeAttribute("id", "btn-menu");
-}
